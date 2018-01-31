cafb5c87723c786db40d0d5ed6c9b0f0b36a96a3
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-31 22:20:54 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,17 +1,19 @@
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style,language|
-    language.each do |detail, value|
-      if language = :javascript
-        new_hash[:language] = {
-          :type => value,
-          :style => [:oo, :functional]
-        }
-      else 
-        new_hash[:language] = {
-          :type => value,
-          :style => [style]
-        }
+    language.each do |name, details|
+      details.each do |type,value|
+       if language = :javascript
+          new_hash[:language] = {
+            :type => value,
+            :style => [:oo, :functional]
+         }
+       else 
+         new_hash[:language] = {
+            :type => value,
+            :style => [style]
+         }
+       end
       end
     end 
   end 

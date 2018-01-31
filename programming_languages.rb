4c70689ceea2bcb0495ba4012203747e10e3a7e6
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-31 22:13:59 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,5 +2,6 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style,language|
     language.each do |detail, value|
-      new_hash[:language] 
+      new_hash[:language] = {
+        :type
 end
